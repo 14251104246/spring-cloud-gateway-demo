1ee88cdd90f6ac24bbb0c2e14e5348ff3f84acfc
zehua <523786283@qq.com>
2018-07-10 09:20:31 +0800

    简单网关路由配置

--- spring-cloud-gateway/src/main/java/com/neo/GatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/neo/GatewayApplication.java
@@ -2,10 +2,19 @@ package com.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+
+@Configuration
 @SpringBootApplication
 public class GatewayApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
